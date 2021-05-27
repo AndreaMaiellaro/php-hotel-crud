96a5b6fbc4a463404f8887b614d5c4bd519ab3cd
shrekt444 <andrea.maie444@gmail.com>
2021-05-27 15:53:43 +0200

    continuazione

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
 <?php 
-    echo('test')
-?>
+    $servername = "localhost";
+    $username = "root";
+    $password = "root";
+    $dbname = "dbhotel";
+    
+    // Connect
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection
+    if ($conn && $conn->connect_error) {
+        echo "Connection failed: " . $conn->connect_error;
+        die();
+    }
+
+    $sql = "SELECT `room_number`, floor FROM `stanze`;";
+    $result = $conn->query($sql);
+
+    if ($result && $result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            echo "Stanza N. ". $row['room_number']. " piano: ". $row['floor'];
+        }
+    }
+?> 
